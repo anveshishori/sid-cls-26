4592565d607588a17d87bb5607b91e794d98d45e
Anveshi Shori <anveshi87@gmail.com>
2021-03-23 14:42:38 +0530

    added platform to code

--- sketch.js
+++ sketch.js
@@ -35,6 +35,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    platform = new Ground(150,300,300,180)
 }
 
 function draw(){
@@ -59,4 +60,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
